c27d455b0ed74fa43b8904448a2753b77daa9a2d
Alissa Likavec <alissalikavec@gmail.com>
2014-08-05 01:44:30 +0000

    CRUD thangs

--- app/controllers/polishes_controller.rb
+++ app/controllers/polishes_controller.rb
@@ -8,8 +8,35 @@ class PolishesController < ApplicationController
   end
 
   def new
+    @polish = Polish.new
+  end
+
+  def create
+    @polish = Polish.new(params.require(:polish).permit(:brand, :name, :color, :style))
+    if @polish.save
+      flash[:notice] = "Polish saved!"
+      redirect_to @polish
+    else
+      flash[:error] = "There was a problem saving your polish. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
   end
 
   def edit
+    @polish = Polish.find(params[:id])
+  end
+
+  def update
+    @polish = Polish.find(params[:id])
+    if @polish.update_attributes(params.require(:polish).permit(:brand, :name, :color, :style))
+      flash[:notice] = "Polish details updated."
+      redirect_to @polish
+    else
+      flash[:error] = "There was an error saving this polish. Please try again!"
+      render :edit
+    end
   end
 end
